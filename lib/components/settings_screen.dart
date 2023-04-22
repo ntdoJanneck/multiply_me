2dc5856a6b13889bf52ca22c896e43fc78aa012c
ntdoJanneck <janneckf03@gmail.com>
2023-04-22 17:04:48 +0200

    implement dark mode switch whoop

--- lib/components/settings_screen.dart
+++ lib/components/settings_screen.dart
@@ -2,6 +2,8 @@ import "dart:developer";
 
 import 'package:flutter/material.dart';
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
+import 'package:multiply_me/theme_provider/theme_model.dart';
+import "package:provider/provider.dart";
 import "../helpers/json_utils.dart";
 import '../helpers/url_helper.dart';
 import "../helpers/imprint_dialog.dart";
@@ -14,8 +16,6 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  ThemeMode theme = ThemeMode.system;
-
   void showConfirmationDialog(
       String title, String content, String answerYes, String answerNo) {
     showDialog(
@@ -42,8 +42,8 @@ class _SettingsScreenState extends State<SettingsScreen> {
     );
   }
 
-  Future<ThemeMode> showThemeDialog(
-      BuildContext context, ThemeMode preset) async {
+  Future<ThemeMode?> showThemeDialog(
+      BuildContext context, ThemeMode preset, ThemeModel themeModel) async {
     ThemeMode? result = await showDialog(
       context: context,
       builder: (BuildContext ctx) {
@@ -92,7 +92,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
           actions: <Widget>[
             TextButton(
                 onPressed: () {
-                  Navigator.pop(context);
+                  Navigator.pop(context, themeModel.theme);
                 },
                 child: const Text("Cancel")),
             TextButton(
@@ -106,10 +106,9 @@ class _SettingsScreenState extends State<SettingsScreen> {
     );
 
     if (result != null) {
-      log(result.toString());
       return result;
     }
-    return ThemeMode.system;
+    return null;
   }
 
   void showAlertBox(String title, String content) {
@@ -133,109 +132,123 @@ class _SettingsScreenState extends State<SettingsScreen> {
   Widget build(BuildContext context) {
     var localization =
         Localizations.of<AppLocalizations>(context, AppLocalizations);
+
+    Map<ThemeMode, String> themeTranslations = {
+      ThemeMode.system: "System",
+      ThemeMode.dark: "Dark",
+      ThemeMode.light: "Light",
+    };
     return Scaffold(
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            ListTile(
-              leading: const Icon(Icons.delete_forever_outlined),
-              title: Text(localization!.settingsDeleteStatisticsDataTitle),
-              onTap: () => {
-                showConfirmationDialog(
-                    localization.multiplicationTableErrorHeadline,
-                    localization
-                        .settingsDeleteStatisticsDataConfirmationMessage,
-                    localization.dialogYes,
-                    localization.dialogNo)
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.brightness_4_outlined),
-              title: const Text("Theme"),
-              subtitle: Text(theme.toString()),
-              onTap: () async {
-                ThemeMode newTheme = await showThemeDialog(context, theme);
-                setState(() {
-                  theme = newTheme;
-                });
-              },
-            ),
-            const Divider(),
-            ListTile(
-              leading: const Icon(Icons.description_outlined),
-              title: Text(localization.settingsViewImprintTitle),
-              onTap: () => ImprintDialog.buildImprint(context),
-            ),
-            ListTile(
-              leading: const Icon(Icons.info_outline),
-              title: Text(localization.settingsAboutViewTitle),
-              onTap: () => {
-                showAboutDialog(
-                  context: context,
-                  applicationName: "MultiplyMe",
-                  applicationLegalese: localization.settingsAboutViewLegalese,
-                  applicationVersion: "1.0.1",
-                  children: [
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    const Divider(),
-                    ListTile(
-                      leading: const Icon(Icons.code_sharp),
-                      title: Text(localization.settingsAboutViewSourceCode),
-                      onTap: () async {
-                        String url =
-                            "https://github.com/ntdoJanneck/multiply_me/";
-                        bool openResult =
-                            await UrlHelper.loadUrl(Uri.parse(url));
-                        if (!openResult) {
-                          showAlertBox(
-                              localization.settingsAboutViewErrorUriHeadline,
-                              localization
-                                  .settingsAboutViewErrorUriContent(url));
-                        }
-                      },
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.shield_outlined),
-                      title: Text(localization.settingsAboutViewPrivacyPolicy),
-                      onTap: () async {
-                        String url =
-                            "https://github.com/ntdoJanneck/multiply_me/blob/main/PRIVACY.md";
-                        bool openResult =
-                            await UrlHelper.loadUrl(Uri.parse(url));
-                        if (!openResult) {
-                          showAlertBox(
-                              localization.settingsAboutViewErrorUriHeadline,
-                              localization
-                                  .settingsAboutViewErrorUriContent(url));
-                        }
-                      },
-                    ),
-                    const Divider(),
-                    ListTile(
-                      leading: const Icon(Icons.font_download_outlined),
-                      title: Text(localization.settingsAboutViewFontLicense),
-                      onTap: () async {
-                        String url =
-                            "https://github.com/googlefonts/roboto/blob/main/LICENSE";
-                        bool openResult =
-                            await UrlHelper.loadUrl(Uri.parse(url));
-                        if (!openResult) {
-                          showAlertBox(
-                              localization.settingsAboutViewErrorUriHeadline,
-                              localization
-                                  .settingsAboutViewErrorUriContent(url));
-                        }
-                      },
-                    )
-                  ],
-                )
-              },
-            ),
-          ],
-        ),
+        child: Consumer<ThemeModel>(builder: (context, themeModel, child) {
+          ThemeMode theme = themeModel.theme;
+          return Column(
+            children: [
+              ListTile(
+                leading: const Icon(Icons.delete_forever_outlined),
+                title: Text(localization!.settingsDeleteStatisticsDataTitle),
+                onTap: () => {
+                  showConfirmationDialog(
+                      localization.multiplicationTableErrorHeadline,
+                      localization
+                          .settingsDeleteStatisticsDataConfirmationMessage,
+                      localization.dialogYes,
+                      localization.dialogNo)
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.brightness_4_outlined),
+                title: const Text("Theme"),
+                subtitle: Text(themeTranslations[theme] ?? "System"),
+                onTap: () async {
+                  ThemeMode? newTheme =
+                      await showThemeDialog(context, theme, themeModel);
+                  if (newTheme != null) {
+                    setState(() {
+                      theme = newTheme;
+                      themeModel.setTheme(theme);
+                    });
+                  }
+                },
+              ),
+              const Divider(),
+              ListTile(
+                leading: const Icon(Icons.description_outlined),
+                title: Text(localization.settingsViewImprintTitle),
+                onTap: () => ImprintDialog.buildImprint(context),
+              ),
+              ListTile(
+                leading: const Icon(Icons.info_outline),
+                title: Text(localization.settingsAboutViewTitle),
+                onTap: () => {
+                  showAboutDialog(
+                    context: context,
+                    applicationName: "MultiplyMe",
+                    applicationLegalese: localization.settingsAboutViewLegalese,
+                    applicationVersion: "1.0.1",
+                    children: [
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Divider(),
+                      ListTile(
+                        leading: const Icon(Icons.code_sharp),
+                        title: Text(localization.settingsAboutViewSourceCode),
+                        onTap: () async {
+                          String url =
+                              "https://github.com/ntdoJanneck/multiply_me/";
+                          bool openResult =
+                              await UrlHelper.loadUrl(Uri.parse(url));
+                          if (!openResult) {
+                            showAlertBox(
+                                localization.settingsAboutViewErrorUriHeadline,
+                                localization
+                                    .settingsAboutViewErrorUriContent(url));
+                          }
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.shield_outlined),
+                        title:
+                            Text(localization.settingsAboutViewPrivacyPolicy),
+                        onTap: () async {
+                          String url =
+                              "https://github.com/ntdoJanneck/multiply_me/blob/main/PRIVACY.md";
+                          bool openResult =
+                              await UrlHelper.loadUrl(Uri.parse(url));
+                          if (!openResult) {
+                            showAlertBox(
+                                localization.settingsAboutViewErrorUriHeadline,
+                                localization
+                                    .settingsAboutViewErrorUriContent(url));
+                          }
+                        },
+                      ),
+                      const Divider(),
+                      ListTile(
+                        leading: const Icon(Icons.font_download_outlined),
+                        title: Text(localization.settingsAboutViewFontLicense),
+                        onTap: () async {
+                          String url =
+                              "https://github.com/googlefonts/roboto/blob/main/LICENSE";
+                          bool openResult =
+                              await UrlHelper.loadUrl(Uri.parse(url));
+                          if (!openResult) {
+                            showAlertBox(
+                                localization.settingsAboutViewErrorUriHeadline,
+                                localization
+                                    .settingsAboutViewErrorUriContent(url));
+                          }
+                        },
+                      )
+                    ],
+                  )
+                },
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
